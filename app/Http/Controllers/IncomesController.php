a5037bf8fd29c5d266245fe75c3a5674096e596d
kasumi sogabe <ksm17sgb@gmail.com>
2023-07-08 00:01:30 +0900

    Incomesコントローラ作成

---
+++ app/Http/Controllers/IncomesController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IncomesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $incomes = Income::all();
+        return view('incomes.index', ['incomes' => $incomes]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('incomes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $income = new Income;
+        $income->fill($request->all())->save();
+        return redirect('/incomes');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+    $income = Income::find($id);
+    return view('incomes.show', ['income' => $income]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+    $income = Income::find($id);
+    return view('incomes.edit', ['income' => $income]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+    $income = Income::find($id);
+    $income->fill($request->all())->save();
+    return redirect('/incomes');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+    $income = Income::find($id);
+    $income->delete();
+    return redirect('/incomes');
+    }
+}
